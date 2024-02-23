d37ce1e691fff4176dd76b0162fd49987a5cc0d9
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-02-23 22:13:04 +0800

    improve compiler message detection algorithm

--- lua/zig-comp-diag/main.lua
+++ lua/zig-comp-diag/main.lua
@@ -1,14 +1,12 @@
 local M = {}
 
 local vim = vim
-
 local zig_comp_diag_ns = vim.api.nvim_create_namespace('zig_comp_diag')
-
 local utils = require('zig-comp-diag.utils')
 
 local prev_comp_diag_by_bufnr = {}
 M.runWithCmd = function(cmd)
-  local handle = io.popen(cmd.." 2>&1")
+  local handle = io.popen(cmd .. " 2>&1")
   if handle == nil then
     print("Failed to run command: " .. vim.g.zig_comp_diag_cmd)
     return
@@ -18,35 +16,50 @@ M.runWithCmd = function(cmd)
 
   local comp_diag_by_bufnr = {}
   for line in output_string:gmatch("[^\r\n]+") do
-    local line_diag = {}
-    for word in string.gmatch(line, "[^:]+") do
-        table.insert(line_diag, word)
-    end
-    if #line_diag == 5 then
-      local bufnr = tonumber(vim.fn.bufadd(line_diag[1]))
-      if comp_diag_by_bufnr[bufnr] == nil then
-        comp_diag_by_bufnr[bufnr] = {}
-      end
+    -- because lua doesn't have continue statement :(
+    local file_name, remain1 = utils.splitStringAtDelimiter(line, ":")
+    if remain1 ~= nil then
+      local lnum, remain2 = utils.splitStringAtDelimiter(remain1, ":")
+      if remain2 ~= nil then
+        local col, remain3 = utils.splitStringAtDelimiter(remain2, ":")
+        if remain3 ~= nil then
+          local severity, message = utils.splitStringAtDelimiter(remain3, ":")
+          if message ~= nil then
+            -- buffer number from file name
+            local bufnr = tonumber(vim.fn.bufadd(file_name))
 
-      table.insert(comp_diag_by_bufnr[bufnr], {
-        bufnr = bufnr,
-        lnum = tonumber(line_diag[2])-1,
-        col = tonumber(line_diag[3]),
-        severity = utils.to_vim_severity(line_diag[4]),
-        message = line_diag[5],
-      })
+            -- initialize the table for buffer number if it doesn't exist
+            if comp_diag_by_bufnr[bufnr] == nil then
+              comp_diag_by_bufnr[bufnr] = {}
+            end
+
+            -- add diagnostic for bufnr
+            table.insert(comp_diag_by_bufnr[bufnr], {
+              bufnr = bufnr,
+              lnum = tonumber(lnum) - 1,
+              col = tonumber(col),
+              severity = utils.toVimSeverity(severity),
+              message = message,
+            })
+          end
+        end
+      end
     end
   end
 
+  -- reset old diagnostics
   for bufnr, _ in pairs(prev_comp_diag_by_bufnr) do
     if comp_diag_by_bufnr[bufnr] == nil then
       vim.diagnostic.reset(zig_comp_diag_ns, bufnr)
     end
   end
 
+  -- set new diagnostics
   for bufnr, diagnostics in pairs(comp_diag_by_bufnr) do
     vim.diagnostic.set(zig_comp_diag_ns, bufnr, diagnostics, {})
   end
+
+  -- save old diagnostics
   prev_comp_diag_by_bufnr = comp_diag_by_bufnr
 end
 
--- lua/zig-comp-diag/utils.lua
+++ lua/zig-comp-diag/utils.lua
@@ -6,19 +6,30 @@ local function trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-M.to_vim_severity = function(zig_comp_severity_untrimmed)
-  local zig_comp_severity = trim(zig_comp_severity_untrimmed)
-  if zig_comp_severity == "error" then
+M.toVimSeverity = function(zigCompSeverityUntrimmed)
+  local zigCompSeverity = trim(zigCompSeverityUntrimmed)
+  if zigCompSeverity == "error" then
     return vim.diagnostic.severity.ERROR
-  elseif zig_comp_severity == "warn" then
+  elseif zigCompSeverity == "warn" then
     return vim.diagnostic.severity.WARN
-  elseif zig_comp_severity == "note" then
+  elseif zigCompSeverity == "note" then
     return vim.diagnostic.severity.INFO
-  elseif zig_comp_severity == "hint" then
+  elseif zigCompSeverity == "hint" then
     return vim.diagnostic.severity.HINT
   else
-    print("Unknown severity: " .. zig_comp_severity)
+    print("Unknown severity: " .. zigCompSeverity)
     return vim.diagnostic.severity.INFO
+  end
+end
+
+M.splitStringAtDelimiter = function(str, delimiter)
+  local delimiterPos = string.find(str, delimiter, 1, true)
+  if delimiterPos then
+    local firstPart = string.sub(str, 1, delimiterPos - 1)
+    local remainingPart = string.sub(str, delimiterPos + 1)
+    return firstPart, remainingPart
+  else
+    return str, nil
   end
 end
 
